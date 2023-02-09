23926d989ffa0233c1968136edcbe5adcd8e9356
Vladan Dimitrijevic <dime.97@hotmail.com>
2023-02-09 17:08:29 +0100

    Creating component RepositoryList and page HomePage

---
+++ src/components/RepositoryList/RepositoryList.js
@@ -0,0 +1,21 @@
+import "./RepositoryList.css";
+
+const RepositoryList = ({data}) => {
+
+  return (
+    <div id="repository-list">
+        <h1>Repository list</h1>
+        {data.length && data.map((item) => (
+            <div id="repository-card" key={item.id}>
+                <div className="repo-card-item" to={`/repository/${item.owner.login}/${item.name}`}>{item.name}</div>
+                <div className="repo-card-item">Stars: {item.stargazers_count}</div>
+                <div className="repo-card-item">Forks: {item.forks_count}</div>
+                <div className="repo-card-item">Username: {item.owner.login}</div>
+                <div className="repo-card-item">Avatar: {item.owner.avatar_url}</div>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default RepositoryList
