c9e54a71d979886a5324c828ddc647f938199470
Vladan Dimitrijevic <dime.97@hotmail.com>
2023-02-09 16:57:48 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header id="header">
+        <ul id="tabs">
+            <li className="tab-item">React</li>
+            <li className="tab-item">Vue</li>
+            <li className="tab-item">Angular</li>
+        </ul>
+        <hr />
+    </header>
+  )
+}
+
+export default Header
