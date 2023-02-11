fcc3e947b6f971d6849b5840a4e9e7566ec53c3e
Vladan Dimitrijevic <dime.97@hotmail.com>
2023-02-11 12:38:43 +0100

    Added Pagination component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export {default as Header}
 from './Header/Header';
 export {default as RepositoryList}
 from './RepositoryList/RepositoryList';
+export {default as Pagination}
+from './Pagination/Pagination';
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,18 @@
 import {useEffect, useState} from 'react'
-import {Header, RepositoryList} from '../../components';
+import {Header, Pagination, RepositoryList} from '../../components';
 import "./HomePage.css";
 
 const HomePage = () => {
     const [tab,
-        setTab] = useState("angular");
+        setTab] = useState("vue");
     const [gitRepos,
         setGitRepos] = useState([]);
     const [currentPage,
         setCurrentPage] = useState(1);
     const [totalRecords,
         setTotalRecords] = useState(0);
-    const [pageNumbers,
-        setPageNumbers] = useState([]);
-    const [active,
-        setActive] = useState(0);
-    const perPage = 10;
 
-    const navigate = (item) => {
-        setActive(item);
-        setCurrentPage(item);
-    }
+    const perPage = 10;
 
     const getGitRepos = async() => {
         const query = `q=${tab}&sort=forks&order=desc&per_page=${perPage}&page=${currentPage}`;
@@ -29,7 +21,6 @@ const HomePage = () => {
         const gitReposFromServer = await fetch(reposUrl);
         const data = await gitReposFromServer.json();
         setTotalRecords(data.total_count);
-        setPageNumbers([...Array(Math.ceil(totalRecords / perPage)).keys()].slice(1));
         setGitRepos(data.items);
     }
 
@@ -50,17 +41,11 @@ const HomePage = () => {
     return (
         <div>
             <Header onClick={(event) => changeTab(event)}/>
-            <RepositoryList data={gitRepos}/>
-            <ul id="repository-list-pagination">
-                {pageNumbers.map((item) => (
-                    <li
-                        key={item}
-                        onClick={() => navigate(item)}
-                        className={active === item
-                        ? 'active'
-                        : ''}>{item}</li>
-                ))}
-            </ul>
+            <RepositoryList data={gitRepos}/> {totalRecords && <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                totalRecords={totalRecords}
+                perPage={perPage}/>}
         </div>
     );
 }
